e4b0ed8e76ceafa1e6da02ce7127ce580aa621a5
40823205 <40823205@mde.tw>
2019-10-22 17:36:08 +0800

    add embedding dartpad

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '\n https://dartpad.dev?id=da3c3adf647b6a3e1990dd9f9edbacc4 \n \n', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'First', 'text': 'About \n \n https://dartpad.dev?id=da3c3adf647b6a3e1990dd9f9edbacc4 \n \n 原始碼 \n int i;\nint sum;\n \nmain(){\n  sum = 0;\n  for(i=1;i <= 10 ;i++){\n    sum += i;\n    print("$sum");\n  }\n  print(\'sum = $sum\');\n} \n \n \n', 'tags': '', 'url': 'First.html'}]};
