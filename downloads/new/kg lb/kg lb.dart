797bfc2dd359c11017180395ef55a41b67add6be
40823205 <40823205@mde.tw>
2020-01-08 23:24:57 +0800

    w17

--- downloads/new/kg lb/kg lb.dart
+++ downloads/new/kg lb/kg lb.dart
@@ -1,17 +1,40 @@
-main(){  
-//字串temp為[40C,80C,50F,40C,25F]
- List temp = [' 25kg', ' 30lb',' 56lb',' 14kg',' 68lb','198kg'];
-  for (var i in temp) {
-  var a = int.parse(i[0]+i[1]+i[2]);
-  //將字串轉數字
-    if (i[4] == "g") {
-    double k=a*(2.2);
-    print("$i=$k Ib");
-}
-    else{ 
-    double m=a*0.45;
-    print("$i=$m Kg");
+LBtoKG(num lb){
 
+return lb*0.45359237;
 }
+
+
+KGtoLB(num kg){
+
+return kg*2.20462262;
 }
-}
+
+
+main() {
+ 
+  int len;
+ 
+  var type;
+  
+  var number;
+  List weight = ["25K", "30L", "56L", "14K", "68L", "198K"];
+  for (var data in weight) {
+    
+    len = data.length;
+    
+    type = data[len-1];
+   
+    number = data.substring(0, len-1);
+   
+    number = int.parse(number);
+   
+    if (type == "L"){
+     
+      print("$number 英磅 =  ${LBtoKG(number).toStringAsFixed(4)} 公斤");
+    }else{
+     
+      print("$number 公斤 =  ${KGtoLB(number).toStringAsFixed(4)} 英磅");
+    }
+      
+  } 
+} 
